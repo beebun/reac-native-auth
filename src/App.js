f0febd7e1114793a91ca29b145a835f7542208f0
Bun Suwanparsert <bun.swp@gmail.com>
2017-10-10 22:57:40 +0700

    Add Spinner and modify login form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentDidMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBV5luH-HLnNKjR0HCWFHcg6UvdHtZxs80',
@@ -15,12 +17,20 @@ class App extends Component {
       storageBucket: 'auth-3cb17.appspot.com',
       messagingSenderId: '240194067327'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 
   render() {
     return (
       <View>
-        <Header headerText="Authentication" />
+        <Header headerText="meGenome" />
         <LoginForm />
       </View>
     );
