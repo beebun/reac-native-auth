508d0652142bc924f415fa92b4e3d7d5f6718214
Bun Suwanparsert <bun.swp@gmail.com>
2017-10-10 23:22:12 +0700

    Add logic to authenticate user

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  state = { loggedIn: false };
+  state = { loggedIn: null };
 
   componentDidMount() {
     firebase.initializeApp({
@@ -27,11 +27,28 @@ class App extends Component {
     });
   }
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true: 
+        return (
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return (<LoginForm />);
+      default:
+        return (<Spinner size='large' />);
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="meGenome" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
